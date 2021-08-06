46a950bf988bf2243c34f489d4e9b2c79679eff1
CHENTing <ting.chen0902@gmail.com>
2021-08-06 10:51:56 +0900

    loading posts and loading users

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -1,32 +1,50 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { PostAuthor } from './PostAuthor';
-import { selectAllPosts } from './postsSlice';
+import { selectAllPosts, fetchPosts } from './postsSlice';
 import { ReactionButtons } from './ReactionButtons';
 import { TimeAgo } from './TimeAgo';
 
 export const PostsList = () => {
+    const dispatch = useDispatch()
     const posts = useSelector(selectAllPosts);
-    const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
 
-    const renderedPosts = orderedPosts.map(post => (
-        <article className="post-excerpt" key={post.id}>
-            <h3>{post.title}</h3>
-            <PostAuthor userId={post.user} />
-            <TimeAgo timestamp={post.date} />
-            <p className="post-content">{post.content.substring(0, 100)}</p>
-            <ReactionButtons post={post} />
-            <Link to={`/posts/${post.id}`} className="button muted-button">
-                View Post
-            </Link>
-        </article>
-    ));
+    const postStatus = useSelector(state => state.posts.status)
+    const error = useSelector(state => state.posts.error)
+
+    useEffect(() => {
+        if (postStatus === 'idle') {
+            dispatch(fetchPosts())
+        }
+    }, [postStatus, dispatch])
+
+    let content
+
+    if (postStatus === 'loading') {
+        content = <div className="loader">Loading...</div>
+    } else if (postStatus === 'succeeded') {
+        const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
+        content = orderedPosts.map(post => (
+            <article className="post-excerpt" key={post.id}>
+                <h3>{post.title}</h3>
+                <PostAuthor userId={post.user} />
+                <TimeAgo timestamp={post.date} />
+                <p className="post-content">{post.content.substring(0, 100)}</p>
+                <ReactionButtons post={post} />
+                <Link to={`/posts/${post.id}`} className="button muted-button">
+                    View Post
+                </Link>
+            </article>
+        ))
+    } else if (postStatus === 'failed') {
+        content = <div>{error}</div>
+    }
 
     return (
         <section className="posts-list">
             <h2>Posts</h2>
-            {renderedPosts}
+            {content}
         </section>
     );
 }
